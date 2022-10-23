b99f09c84c6cca7a73e58929b74b920962bf7250
Alexandra Eliseeva <klen.sasha_22@list.ru>
2022-10-23 22:23:14 +0300

    solution

---
+++ hw4 anagrams.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+s = list(input().lower().strip().replace(' ', ''))
+p = list(input().lower().strip().replace(' ', ''))
+
+chars_s = defaultdict(int, {char_s: s.count(char_s) for char_s in s})
+chars_p = {char_p: p.count(char_p) for char_p in p}
+
+indices = set()
+
+for char_p, freq_p in chars_p.items():
+    if chars_s[char_p] >= freq_p:
+
+        start_idx = 0
+        end_idx = len(p)
+        while end_idx <= len(s):
+
+            if sorted(p) == sorted(s[start_idx:end_idx]):
+                indices.add(start_idx)
+
+            start_idx += 1
+            end_idx += 1
+
+print(list(indices) if indices else 'no anagrams :(')
