87536ca76e6b19d5157bd3bca44d73e6f474e40f
Alexandra Eliseeva <klen.sasha_22@list.ru>
2022-10-23 22:33:31 +0300

    made a func

--- hw4 anagrams.py
+++ hw4 anagrams.py
@@ -1,24 +1,31 @@
 from collections import defaultdict
 
-s = list(input().lower().strip().replace(' ', ''))
-p = list(input().lower().strip().replace(' ', ''))
 
-chars_s = defaultdict(int, {char_s: s.count(char_s) for char_s in s})
-chars_p = {char_p: p.count(char_p) for char_p in p}
+def find_anagrams(s: str, p: str):
+    s = list(s.lower().strip().replace(' ', ''))
+    p = list(p.lower().strip().replace(' ', ''))
 
-indices = set()
+    chars_s = defaultdict(int, {char_s: s.count(char_s) for char_s in s})
+    chars_p = {char_p: p.count(char_p) for char_p in p}
 
-for char_p, freq_p in chars_p.items():
-    if chars_s[char_p] >= freq_p:
+    indices = set()
 
-        start_idx = 0
-        end_idx = len(p)
-        while end_idx <= len(s):
+    for char_p, freq_p in chars_p.items():
+        if chars_s[char_p] >= freq_p:
 
-            if sorted(p) == sorted(s[start_idx:end_idx]):
-                indices.add(start_idx)
+            start_idx = 0
+            end_idx = len(p)
+            while end_idx <= len(s):
 
-            start_idx += 1
-            end_idx += 1
+                if sorted(p) == sorted(s[start_idx:end_idx]):
+                    indices.add(start_idx)
 
-print(list(indices) if indices else 'no anagrams :(')
+                start_idx += 1
+                end_idx += 1
+
+    print(list(indices) if indices else 'no anagrams :(')
+
+
+find_anagrams('qrwetyqoiwuy', 'cvm')
+find_anagrams('cbaebabacd', 'abc')
+find_anagrams('dEBIt  cArd', 'BAD credit    ')
