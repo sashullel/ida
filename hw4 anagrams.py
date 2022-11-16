dcf730f55827e53fcfdfe586a6d9a5f146f43182
Alexandra Eliseeva <klen.sasha_22@list.ru>
2022-11-16 17:10:18 +0300

    fixed

--- hw4 anagrams.py
+++ hw4 anagrams.py
@@ -2,30 +2,23 @@ from collections import defaultdict
 
 
 def find_anagrams(s: str, p: str):
-    s = list(s.lower().strip().replace(' ', ''))
-    p = list(p.lower().strip().replace(' ', ''))
+    s = s.lower().strip().replace(' ', '')
+    p = p.lower().strip().replace(' ', '')
 
-    chars_s = defaultdict(int, {char_s: s.count(char_s) for char_s in s})
-    chars_p = {char_p: p.count(char_p) for char_p in p}
+    chars_p = defaultdict(int, {char_p: p.count(char_p) for char_p in p})
 
-    indices = set()
+    indices = []
+    i, j = 0, len(p)
+    while j <= len(s):
+        if all(char_p in s[i:j] for char_p in p) and all(s[i:j].count(char_s) >= chars_p[char_s] for char_s in s[i:j]):
+            indices.append(i)
+        i += 1
+        j += 1
 
-    for char_p, freq_p in chars_p.items():
-        if chars_s[char_p] >= freq_p:
-
-            start_idx = 0
-            end_idx = len(p)
-            while end_idx <= len(s):
-
-                if sorted(p) == sorted(s[start_idx:end_idx]):
-                    indices.add(start_idx)
-
-                start_idx += 1
-                end_idx += 1
-
-    print(list(indices) if indices else 'no anagrams :(')
+    print(indices if indices else 'no anagrams :(')
 
 
-find_anagrams('qrwetyqoiwuy', 'cvm')
-find_anagrams('cbaebabacd', 'abc')
-find_anagrams('dEBIt  cArd', 'BAD credit    ')
+find_anagrams('qrwetyqoiwuy', 'cvm')  # no anagrams :(
+find_anagrams('cbaebabacd', 'abc')  # [0, 6]
+find_anagrams('cbaebabacd', 'abcc')  # no anagrams :(
+find_anagrams('dEBIt  cArd', 'BAD credit    ')  # [0]
