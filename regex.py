3c61171073d9447a0358a1e96e1a0ec91acf7134
Alexandra Eliseeva <klen.sasha_22@list.ru>
2022-11-21 12:46:56 +0300

    .

--- regex.py
+++ regex.py
@@ -4,12 +4,16 @@ import string
 
 
 def generate_password(n: int):
+    if n < 4:
+        print('password must contain at least 4 characters')
+        return None
     cyrillic = 'АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя'
     symbols = list(string.ascii_letters + string.digits + string.punctuation + cyrillic)
     # в символах не задан пробел, поэтому по сути пробельные знаки в паттерне необязательно исключать?
     pattern = r"^(?=.*[A-ZА-ЯЁ]{2,})(?=.*[a-zа-яё]{2,})(?=.*[A-ЯЁа-яёA-Za-z])(?=.*[0-9])(?!.*[0-9]{3})(?!.*[\s]).*$"
 
     valid_password = None
+    print('generating a password..')
     while not valid_password:
         password = ''.join(random.choices(symbols, k=n))
         valid_password = re.findall(pattern, password)
@@ -17,4 +21,5 @@ def generate_password(n: int):
     print(*valid_password)
 
 
-generate_password(12)
+generate_password(3)  # password must contain at least 4 characters
+generate_password(12)  # ЩИы"%9элт4)$
